66579058bac610221061bd43d56136e61da74e48
Mitch <mitch@tabian.ca>
2018-05-29 14:26:41 -0700

    2.1_start

--- app/src/main/java/codingwithmitch/com/tabiancustomcamera/MainActivity.java
+++ app/src/main/java/codingwithmitch/com/tabiancustomcamera/MainActivity.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.content.pm.PackageManager;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v4.content.ContextCompat;
@@ -16,10 +17,12 @@ import android.view.View;
 public class MainActivity extends AppCompatActivity{
 
     private static final String TAG = "MainActivity";
+    private static final int REQUEST_CODE = 1234;
 
     //widgets
 
     //vars
+    private boolean mPermissions;
 
 
     @Override
@@ -27,11 +30,66 @@ public class MainActivity extends AppCompatActivity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
+        init();
     }
 
+    private void init(){
+        if(mPermissions){
+            if(checkCameraHardware(this)){
 
+                // Open the Camera
+                Log.d(TAG, "init: opening the camera fragment.");
+            }
+        }
+        else{
+            verifyPermissions();
+        }
+    }
 
+    /** Check if this device has a camera */
+    private boolean checkCameraHardware(Context context) {
+        if (context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA)){
+            // this device has a camera
+            return true;
+        } else {
+            // no camera on this device
+            return false;
+        }
+    }
+
+    public void verifyPermissions(){
+        Log.d(TAG, "verifyPermissions: asking user for permissions.");
+        String[] permissions = {
+                android.Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                Manifest.permission.CAMERA};
+        if (ContextCompat.checkSelfPermission(this.getApplicationContext(),
+                permissions[0] ) == PackageManager.PERMISSION_GRANTED
+                && ContextCompat.checkSelfPermission(this.getApplicationContext(),
+                permissions[1] ) == PackageManager.PERMISSION_GRANTED) {
+            mPermissions = true;
+            init();
+        } else {
+            ActivityCompat.requestPermissions(
+                    MainActivity.this,
+                    permissions,
+                    REQUEST_CODE
+            );
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if(requestCode == REQUEST_CODE){
+            if(mPermissions){
+                init();
+            }
+            else{
+                verifyPermissions();
+            }
+        }
+    }
 }
 
 
