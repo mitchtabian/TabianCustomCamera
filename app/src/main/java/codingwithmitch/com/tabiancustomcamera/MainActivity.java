c56ee742e8823d1c310702fa3c2748c8b60b57b8
Mitch <mitch@tabian.ca>
2018-05-29 14:21:30 -0700

    first

--- app/src/main/java/codingwithmitch/com/tabiancustomcamera/MainActivity.java
+++ app/src/main/java/codingwithmitch/com/tabiancustomcamera/MainActivity.java
@@ -1,17 +1,27 @@
-package codingwithmitch.com.tabiancustomcamera;
+package codingwithmitch.com.customcameratest;
 
 import android.Manifest;
+import android.app.ActionBar;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.graphics.drawable.Drawable;
-import android.os.Bundle;
+import android.hardware.Camera;
+import android.os.Build;
+import android.support.annotation.MainThread;
+import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+
+import java.io.File;
 
 public class MainActivity extends AppCompatActivity implements IMainActivity{
 
@@ -24,7 +34,7 @@ public class MainActivity extends AppCompatActivity implements IMainActivity{
     //widgets
 
     //vars
-    private boolean mStoragePermissions;
+    private boolean mPermissions;
     public String mCameraOrientation = "10"; // Front-facing or back-facing
 
 
@@ -66,7 +76,7 @@ public class MainActivity extends AppCompatActivity implements IMainActivity{
     }
 
     private void init(){
-        if(mStoragePermissions){
+        if(mPermissions){
             if(checkCameraHardware(this)){
 
                 // ... CHECK SAVED INSTANCE STATE??
@@ -169,13 +179,13 @@ public class MainActivity extends AppCompatActivity implements IMainActivity{
     public void verifyPermissions(){
         Log.d(TAG, "verifyPermissions: asking user for permissions.");
         String[] permissions = {
-                Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                android.Manifest.permission.WRITE_EXTERNAL_STORAGE,
                 Manifest.permission.CAMERA};
         if (ContextCompat.checkSelfPermission(this.getApplicationContext(),
                 permissions[0] ) == PackageManager.PERMISSION_GRANTED
                 && ContextCompat.checkSelfPermission(this.getApplicationContext(),
                 permissions[1] ) == PackageManager.PERMISSION_GRANTED) {
-            mStoragePermissions = true;
+            mPermissions = true;
             init();
         } else {
             ActivityCompat.requestPermissions(
@@ -183,6 +193,24 @@ public class MainActivity extends AppCompatActivity implements IMainActivity{
                     permissions,
                     REQUEST_CODE
             );
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        Log.d(TAG, "onRequestPermissionsResult: called.");
+
+        if(requestCode == REQUEST_CODE){
+            Log.d(TAG, "onRequestPermissionsResult: request code detected.");
+            if(mPermissions){
+                Log.d(TAG, "onRequestPermissionsResult: storage permission is granted.");
+                init();
+            }
+            else{
+                verifyPermissions();
+            }
         }
     }
 
